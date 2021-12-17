b60b5c8b1660fb5ca4e5829095e25f2436b655ff
Udoka Okoye <leviokoye@gmail.com>
2021-12-16 21:03:48 -0500

    Initial Commit

---
+++ components/Sidebar.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import {Avatar, IconButton} from '@material-ui/core'
+import ChatIcon from '@material-ui/icons/chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+function Sidebar() {
+    return (
+        <Container>
+            <Header>
+                <UserAvatar />
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </IconsContainer>
+            </Header>
+        </Container>
+    )
+}
+
+export default Sidebar
+
+const Container = styled.div`
+
+`;
+
+const Header = styled.div`
+display: flex;
+position: sticky;
+top: 0;
+background-color: white;
+z-index: 1;
+justify-content: space-between;
+align-items: center;
+padding: 15px;
+height: 88px;
+border-bottom: 1px solid whitesmoke;
+`;
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+`;
+
+const IconsContainer = styled.div`
+
+`;
